7d1f3198f3f4cbc74f55f9013cd54e8500c625f6
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-05-25 13:50:36 -0400

    Setup Table View

---
+++ ToDo/ToDoListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoListViewController.swift
+//  ToDo
+//
+//  Created by Jagdev Singh Jhajj on 2020-05-25.
+//  Copyright © 2020 Jagdev Singh Jhajj. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+
+}

d8d88d8738df02c2826098e5f23e6d6c20f06304
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-05-25 14:59:23 -0400

    Implement Persistent Local Data Storage Using UserDefaults

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoListArray"){
+            itemArray = items as! [String]
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -58,6 +64,8 @@ class ToDoListViewController: UITableViewController {
                 
         let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()
         }

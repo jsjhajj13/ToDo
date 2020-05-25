34d8bac73a44f93f9565a220d08bfd7e5bbf29df
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-05-25 14:14:28 -0400

    Add Checkmark

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -32,4 +32,18 @@ class ToDoListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
